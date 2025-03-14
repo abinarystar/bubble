d40d82de8d0cfa50c299b9ab5b57a5d946e343c7
Bryan Arista <bryan.arista@gdn-commerce.com>
2025-03-14 17:43:02 +0700

    refactor: MailService bean creation

--- src/main/java/com/abinarystar/core/mail/MailAutoConfiguration.java
+++ src/main/java/com/abinarystar/core/mail/MailAutoConfiguration.java
@@ -1,16 +1,17 @@
 package com.abinarystar.core.mail;
 
 import com.abinarystar.core.template.TemplateService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.MailSender;
 
-@AutoConfiguration
+@AutoConfiguration(after = MailSenderAutoConfiguration.class)
 @EnableConfigurationProperties(AbinarystarMailProperties.class)
 @PropertySource("classpath:mail.properties")
 public class MailAutoConfiguration {
@@ -18,17 +19,15 @@ public class MailAutoConfiguration {
   @Bean
   @ConditionalOnMissingBean
   @ConditionalOnProperty(name = "abinarystar.core.mail.enabled", havingValue = "false", matchIfMissing = true)
-  public MailService dummyMailService(@Autowired(required = false) TemplateService templateService) {
+  public MailService dummyMailService(TemplateService templateService) {
     return new DummyMailService(templateService);
   }
 
   @Bean
+  @ConditionalOnBean(MailSender.class)
   @ConditionalOnMissingBean
   @ConditionalOnProperty("abinarystar.core.mail.enabled")
-  public MailService simpleMailService(
-      MailSender mailSender,
-      @Autowired(required = false) TemplateService templateService
-  ) {
+  public MailService simpleMailService(MailSender mailSender, TemplateService templateService) {
     return new SimpleMailService(mailSender, templateService);
   }
 }

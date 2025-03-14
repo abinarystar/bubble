22efe0de0ee7e23c45180a167d7af4579aefd706
Bryan Arista <bryan.arista@gdn-commerce.com>
2025-03-14 19:11:40 +0700

    feat: rewrite MailService bean creation without @ConditionalOnProperty

--- src/main/java/com/abinarystar/core/mail/MailAutoConfiguration.java
+++ src/main/java/com/abinarystar/core/mail/MailAutoConfiguration.java
@@ -1,9 +1,9 @@
 package com.abinarystar.core.mail;
 
 import com.abinarystar.core.template.TemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -16,15 +16,14 @@ public class MailAutoConfiguration {
 
   @Bean
   @ConditionalOnMissingBean
-  @ConditionalOnProperty(name = "abinarystar.core.mail.enabled", havingValue = "false", matchIfMissing = true)
-  public MailService dummyMailService(TemplateService templateService) {
+  public MailService mailService(
+      AbinarystarMailProperties mailProperties,
+      @Autowired(required = false) JavaMailSender mailSender,
+      TemplateService templateService
+  ) {
+    if (mailProperties.isEnabled()) {
+      return new SimpleMailService(mailSender, templateService);
+    }
     return new DummyMailService(templateService);
-  }
-
-  @Bean
-  @ConditionalOnMissingBean
-  @ConditionalOnProperty("abinarystar.core.mail.enabled")
-  public MailService simpleMailService(JavaMailSender mailSender, TemplateService templateService) {
-    return new SimpleMailService(mailSender, templateService);
   }
 }

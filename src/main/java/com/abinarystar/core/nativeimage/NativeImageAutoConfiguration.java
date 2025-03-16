127d0a6d52cfe464c2f9a3d096363b4ab76967b9
Bryan Arista <bryan.arista@gdn-commerce.com>
2025-03-16 21:12:24 +0700

    feat: use static native image hint

--- src/main/java/com/abinarystar/core/nativeimage/NativeImageAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.abinarystar.core.nativeimage;
-
-import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.context.annotation.ImportRuntimeHints;
-
-@AutoConfiguration
-@ImportRuntimeHints(NativeImageRuntimeHintsRegistrar.class)
-public class NativeImageAutoConfiguration {
-}
